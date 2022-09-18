6f849dc2d34fb527b619162f6b9427c33502f52a
HxGned <dengxionghui@hotmail.com>
2022-09-18 14:33:10 +0800

    MyReactor: add skeleton for PollPoller

--- net/PollPoller.cc
+++ net/PollPoller.cc
@@ -1,1 +1,15 @@
 #include "PollPoller.hh"
+
+using namespace net;
+using namespace base;
+using namespace std;
+
+PollPoller::PollPoller(EventLoop *loop) : Poller(loop), _eventsNum(0)
+{
+
+}
+
+PollPoller::~PollPoller()
+{
+
+}
--- net/PollPoller.hh
+++ net/PollPoller.hh
@@ -1,4 +1,29 @@
 #ifndef __POLL_POLLER_HH__
 #define __POLL_POLLER_HH__
 
+#include "Poller.hh"
+#include <sys/poll.h>
+
+#define POLL_SIZE 1024
+
+namespace net {
+
+class PollPoller : public Poller {
+public:
+    PollPoller(EventLoop *loop);
+    ~PollPoller();
+
+    // implement interfaces
+    virtual Timestamp Poll(int timeoutMs, ChannelList *activeChannels);
+    virtual void UpdateChannel(Channel *channel);
+    virtual void RemoveChannel(Channel *channel);
+private:
+    void FillActiveChannels(ChannelList *channelList);
+private:
+    int _eventsNum;
+    pollfd fds[POLL_SIZE];
+};
+
+};
+
 #endif
