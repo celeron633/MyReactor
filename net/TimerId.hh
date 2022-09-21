a9c67968f767cd49275a3dab2b6fe23f5a2881d1
HxGned <dengxionghui@hotmail.com>
2022-09-21 23:15:04 +0800

    MyReactor: implement TimerId

--- net/TimerId.hh
+++ net/TimerId.hh
@@ -1,4 +1,39 @@
 #ifndef __TIMER_ID_HH__
 #define __TIMER_ID_HH__
 
+#include "Timer.hh"
+#include "TimerQueue.hh"
+
+namespace net {
+
+// wrapper for 'Timer'
+class TimerId {
+    friend class TimerQueue;
+public:
+    TimerId() : _timerPtr(NULL), _sequence(0)
+    {
+
+    }
+
+    TimerId(Timer* t, int64_t seq) : _timerPtr(t), _sequence(seq)
+    {
+
+    }
+
+    Timer* getTimer()
+    {
+        return _timerPtr;
+    }
+
+    int64_t getSeq()
+    {
+        return _sequence;
+    }
+private:
+    Timer* _timerPtr;
+    int64_t _sequence;
+};
+
+}
+
 #endif
