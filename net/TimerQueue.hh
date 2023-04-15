efb1d3462610e990ec0013d712ec8cc5ed865f9c
HxGned <dengxionghui@hotmail.com>
2023-04-15 09:32:25 +0800

    Timer: move TimerId class into TimerQueue.hh

--- net/TimerQueue.hh
+++ net/TimerQueue.hh
@@ -16,6 +16,42 @@ class EventLoop;
 
 typedef pair<Timestamp, Timer*> TimerEntry;
 typedef list<TimerEntry> TimerList;
+using TimerSeqType = int64_t;
+class Timer;
+
+// wrapper for 'Timer'
+class TimerId {
+    friend class TimerQueue;
+public:
+    TimerId() : _timerPtr(NULL), _sequence(0)
+    {
+
+    }
+
+    TimerId(Timer* t, int64_t seq) : _timerPtr(t), _sequence(seq)
+    {
+
+    }
+
+    Timer* getTimer()
+    {
+        return _timerPtr;
+    }
+
+    int64_t getSeq()
+    {
+        return _sequence;
+    }
+
+    void makeInvalid()
+    {
+        this->_sequence = -1;
+        this->_timerPtr = NULL;
+    }
+private:
+    Timer* _timerPtr;
+    int64_t _sequence;
+};
 
 class TimerQueue {
 public:
